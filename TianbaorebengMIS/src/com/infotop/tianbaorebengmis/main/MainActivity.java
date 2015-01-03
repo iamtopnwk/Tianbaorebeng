be49b0b340a5858bb58ec57ea5fe797f2c59e6be
Balchender <balchender.nakkala@gmail.com>
2015-01-03 09:30:16 +0800

    i have modified in main

--- TianbaorebengMIS/src/com/infotop/tianbaorebengmis/main/MainActivity.java
+++ TianbaorebengMIS/src/com/infotop/tianbaorebengmis/main/MainActivity.java
@@ -1,18 +1,68 @@
 package com.infotop.tianbaorebengmis.main;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.example.tianbaorebengmis.R;
 
+
+import com.infotop.tianbaorebengmis.httpservice.HttpServiceHandler;
+import com.infotop.tianbaorebengmis.httpservice.HttpUrl;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG_ID = "id";
+	private static final String TAG_USERNAME = "userName";
+	private static final String TAG_DEVICENAME = "deviceName";
+	private static final String TAG_UUID = "uuid";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		String serverURL = new HttpUrl().getUrl()+":8080/tianMIS/rest/devicesList";
+
+		// Use AsyncTask execute Method To Prevent ANR Problem
+		new LongOperation().execute(serverURL);
+	}
+	private class LongOperation extends AsyncTask<String, Void, Void> {
+
+		@Override
+		protected Void doInBackground(String... urls) {
+		    String pcontent;
+			// Send data
+			try {
+				HttpServiceHandler hs = new HttpServiceHandler();
+				pcontent = hs.httpContent(urls[0]);
+				System.out.println("*********Content*******");	
+				System.out.println(pcontent);
+				 JSONArray jsonArray;
+					jsonArray = new JSONArray(pcontent);
+					
+					int length=jsonArray.length();
+					for(int i=0;i<length;i++){
+						JSONObject pc=jsonArray.getJSONObject(i);
+						System.out.println(pc.getString(TAG_DEVICENAME));
+						System.out.println(pc.getString(TAG_USERNAME));
+						System.out.println(pc.getString(TAG_ID));
+					}
+					
+			} catch (Exception ex) {
+				System.out.println("Exception e:" + ex.getMessage());
+			}
+			/*****************************************************/
+			return null;
+		}
+
+		protected void onPostExecute(Void unused) {
+			
+		}
 	}
 
 	@Override
